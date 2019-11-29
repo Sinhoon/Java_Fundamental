22d952712c299547fbaa83697087e940a8541705
SinHoon <SinHoon>
2019-11-29 10:48:42 +0900

    v2

---
+++ src/java_20191129/TryCatchDemo.java
@@ -0,0 +1,12 @@
+package java_20191129;
+
+public class TryCatchDemo {
+	public static double getAvg(int k , int e){
+		int sum = k+ e;
+		double avg = (double)sum/(double)2;
+		return avg;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
